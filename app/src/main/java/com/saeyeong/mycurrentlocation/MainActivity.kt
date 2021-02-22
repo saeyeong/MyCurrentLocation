694717c3bfa581dd1e82f138b0b4a7ddf9a75ddc
saeyeong <syeongppp@gmail.com>
2021-02-22 16:12:35 +0900

    [ADD] 현재 위치 출력 기능 추가

--- app/src/main/java/com/saeyeong/mycurrentlocation/MainActivity.kt
+++ app/src/main/java/com/saeyeong/mycurrentlocation/MainActivity.kt
@@ -1,11 +1,150 @@
 package com.saeyeong.mycurrentlocation
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.Task
+import com.google.android.material.snackbar.Snackbar
+import com.saeyeong.mycurrentlocation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val fusedLocationClient: FusedLocationProviderClient by lazy {
+        LocationServices.getFusedLocationProviderClient(applicationContext)
+    }
+    private var cancellationTokenSource = CancellationTokenSource()
+
+    private val fineLocationRationalSnackbar by lazy {
+        Snackbar.make(
+            binding.container,
+            R.string.fine_location_permission_rationale,
+            Snackbar.LENGTH_LONG
+        ).setAction(R.string.ok) {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_FINE_LOCATION_PERMISSIONS_REQUEST_CODE
+            )
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        cancellationTokenSource.cancel()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        Log.d(TAG, "onRequestPermissionResult()")
+
+        if (requestCode == REQUEST_FINE_LOCATION_PERMISSIONS_REQUEST_CODE) {
+            when {
+                grantResults.isEmpty() ->
+                    Log.d(TAG, "User interaction was cancelled.")
+
+                grantResults[0] == PackageManager.PERMISSION_GRANTED ->
+                    Snackbar.make(
+                        binding.container,
+                        R.string.permission_approved_explanation,
+                        Snackbar.LENGTH_LONG
+                    )
+                        .show()
+
+                else -> {
+                    Snackbar.make(
+                        binding.container,
+                        R.string.fine_permission_denied_explanation,
+                        Snackbar.LENGTH_LONG
+                    )
+                        .setAction(R.string.settings) {
+                            val intent = Intent()
+                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            val uri = Uri.fromParts(
+                                "package",
+                                BuildConfig.APPLICATION_ID,
+                                null
+                            )
+                            intent.data = uri
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            startActivity(intent)
+                        }
+                        .show()
+                }
+            }
+        }
+    }
+
+    fun locationRequestOnClick(view: View) {
+        Log.d(TAG, "locationRequestOnClick()")
+
+        val permissionApproved =
+            applicationContext.hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (permissionApproved) {
+            requestCurrentLocation()
+        } else {
+            requestPermissionWithRationale(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                REQUEST_FINE_LOCATION_PERMISSIONS_REQUEST_CODE,
+                fineLocationRationalSnackbar
+            )
+        }
+    }
+    @SuppressLint("MissingPermission")
+    private fun requestCurrentLocation() {
+        Log.d(TAG, "requestCurrentLocation()")
+        if (applicationContext.hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            val currentLocationTask: Task<Location> = fusedLocationClient.getCurrentLocation(
+                PRIORITY_HIGH_ACCURACY,
+                cancellationTokenSource.token
+            )
+
+            currentLocationTask.addOnCompleteListener { task: Task<Location> ->
+                if (task.isSuccessful && task.result != null) {
+                    logOutputToScreen(task.result)
+                } else {
+                    Log.d(TAG, "Location (failure): ${task.exception}")
+                }
+            }
+        }
+    }
+
+    private fun logOutputToScreen(result: Location) {
+        binding.run {
+            tvNumLatitude.text = "${result.latitude}"
+            tvNumLongitude.text = "${result.longitude}"
+            tvNumAltitude.text = "${result.altitude}"
+            tvNumAccuracy.text = "${result.accuracy}"
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val REQUEST_FINE_LOCATION_PERMISSIONS_REQUEST_CODE = 34
     }
 }
